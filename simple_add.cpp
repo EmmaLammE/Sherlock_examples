074023835f7bc168ef084986111e0affc792899a
Emma Liu <emmaliu@DN0a226709.SUNet>
2023-02-28 08:55:45 -0800

    simple addition example

---
+++ simple_add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double a, b;
+    double my_sum;
+    a = 2.0;
+    b = 3.2;
+    my_sum = a+b;
+    cout<<" a = "<<a<<", b = "<<b<<", a+b = "<<my_sum<<endl;
+
+    return 0.0;
+}
